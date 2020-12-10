f165c6f8a26dd6bf3224fbedbef488d642841884
gabrielphp2020 <75779050+gabrielphp2020@users.noreply.github.com>
2020-12-10 10:59:12 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const USER = "tetradogpwa";
+const USER = "gabrielphp2020";
 const ROOTUTILS = "https://" + USER + ".github.io/Utils/";
 
 
-const CACHE_VERSION_ANTERIOR = 35; //subo aqui para no tener problemas :D
+const CACHE_VERSION_ANTERIOR = 37; //subo aqui para no tener problemas :D
 const CACHE_VERSION = CACHE_VERSION_ANTERIOR + 1;
 const APP = "SQLTest";
 const CACHE_INMUTABLE = "CACHE_INMUTABLE_" + APP;
